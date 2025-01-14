c44ca4a31956142b6fba6338f20518caab258d64
shahid2000 <shahid@teamatix.io>
2025-01-14 22:02:10 +0300

    port

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 
   useEffect(() => {
     async function fetchData(): Promise<void> {
-      const response = await fetch('http://localhost:8000/tickets?_sort=price_rub&_order=desc');
+      const response = await fetch('http://localhost:8090/tickets?_sort=price_rub&_order=desc');
       const result = await response.json();
       let filteredTickets: Ticket[];
 
